c20b6b21563a980ee6ec18259adbe977fe40c891
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2013-12-22 13:04:08 +0100

    Rails _3.2.14_ new Dicto-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DictoRails::Application.config.secret_token = '40083ede3f80bee3ec891d08f3a758bf9096339b9aa282aca88b01b4f2fa281b53305374e88c16ee5521ffbed8976c86ec75b91d5f89d9bd013f0d76fd92ee94'
