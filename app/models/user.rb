77f7eb846004bf481b7b560a0cedc56db212008c
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2013-12-23 20:15:46 +0100

    Enable sign up (attr_accessible)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [ :facebook ]
+  attr_accessible :email, :password, :password_confirmation
 end
