dcf066212ed375bb9401fb3a21c4067875791934
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2013-12-24 16:10:21 +0100

    Associations foreign keys names

--- db/migrate/20131224124009_add_foreign_keys_to_associations.rb
+++ db/migrate/20131224124009_add_foreign_keys_to_associations.rb
@@ -2,14 +2,16 @@ class AddForeignKeysToAssociations < ActiveRecord::Migration
   def up
     change_table :associations do |t|
       t.foreign_key :association_types, dependent: :delete
-      t.foreign_key :words, column: 'first_word_id', dependent: :delete
-      t.foreign_key :words, column: 'second_word_id', dependent: :delete
+      t.foreign_key :words, column: 'first_word_id', name: 'associations_first_word_id_fk', dependent: :delete
+      t.foreign_key :words, column: 'second_word_id', name: 'associations_second_word_id_fk', dependent: :delete
     end
   end
 
   def down
     change_table :associations do |t|
-      t.remove_foreign_key :words
+      t.remove_foreign_key :association_types
+      t.remove_foreign_key name: 'associations_first_word_id_fk'
+      t.remove_foreign_key name: 'associations_second_word_id_fk'
     end
   end
 end
