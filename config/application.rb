b112230cef37f68f3066264e6202135f3c064c00
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2013-12-24 13:11:34 +0100

    Factory Girl

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,13 @@ module DictoRails
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+
+      g.view_specs false
+      g.helper_specs false
+    end
   end
 end
