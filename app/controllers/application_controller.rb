81f5077f3967ad92bdc504375490458d396df023
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2013-12-24 17:17:47 +0100

    rescue_from 403

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
     strategy DecentExposure::StrongParametersStrategy
   end
 
+  rescue_from CanCan::AccessDenied do |exception|
+    logger.error exception
+    render 'home/403'
+  end
+
   rescue_from ActiveRecord::RecordNotFound do |exception|
     logger.error exception
     render 'home/404'
