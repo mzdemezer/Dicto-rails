092d4d5d28e7c16d376c724c011a0f64ddd50370
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2013-12-25 15:58:31 +0100

    WordSet#permits_read_by? spec

---
+++ spec/models/word_set_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe WordSet do
+  let(:word_set) { create(:word_set) }
+  let(:read_user) { word_set.user_word_sets.find { |uws| uws.permissions == 'r' }.user }
+  let(:write_user) { word_set.user_word_sets.find { |uws| uws.permissions == 'w' }.user }
+  let(:other_user) { build(:user) }
+
+  describe '#permits_read_by?' do
+    it { expect(word_set).to respond_to(:permits_read_by?) }
+
+    it 'permits to be read by user with read permissions' do
+      expect(word_set.permits_read_by?(read_user)).to eq(true)
+    end
+
+    it 'permits to be read by user with write permissions' do
+      expect(word_set.permits_read_by?(write_user)).to eq(true)
+    end
+
+    it 'does not permit to be read by other users' do
+      expect(word_set.permits_read_by?(other_user)).to eq(false)
+    end
+  end
+end
