ee38c13e06f780e58b3f474fbd4796d4006f1398
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-02-01 12:23:50 +0100

    search_tabs controller

--- app/controllers/search_tabs_controller.rb
+++ app/controllers/search_tabs_controller.rb
@@ -2,7 +2,35 @@ class SearchTabsController < ApplicationController
   respond_to :json
 
   expose(:search_tabs) { current_user.search_tabs }
+  expose(:search_tab, attributes: :search_tab_params)
 
   def index
+  end
+
+  def create
+    save_search_tab
+  end
+
+  def update
+    save_search_tab
+  end
+
+  def destroy
+    search_tab.destroy
+    render json: {}
+  end
+
+  private
+
+  def save_search_tab
+    if search_tab.save
+      render 'search_tabs/show'
+    else
+      respond_with search_tab
+    end
+  end
+
+  def search_tab_params
+    params.require(:search_tab).permit(:scheme_prefix, :scheme_suffix)
   end
 end
