772e9092fdd0f9ef2727cd8bf36832ce584b935a
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-02-07 23:45:48 +0100

    categories_controller implementation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,8 +3,35 @@ class CategoriesController < ApplicationController
 
   expose(:word_set)
   expose(:categories) { word_set.categories }
+  expose(:category, attributes: :category_params)
 
   def index
   end
 
+  def update
+    save_category
+  end
+
+  def create
+    save_category
+  end
+
+  def destroy
+    category.destroy
+    render json: {}
+  end
+
+  private
+
+  def save_category
+    if category.save
+      render 'categories/show'
+    else
+      respond_with category
+    end
+  end
+
+  def category_params
+    params.permit(:name, :word_set_id, :parent_id)
+  end
 end
