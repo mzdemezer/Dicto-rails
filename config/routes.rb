12306cac8533cfd731581fb997a7d163174ab923
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-02-08 22:39:40 +0100

    Point app to root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ DictoRails::Application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'home#index'
 
-  get 'search', to: 'search#index'
-
   resources :word_sets do
     resources :words do
       member do
