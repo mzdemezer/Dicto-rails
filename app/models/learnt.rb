af769b3f9dea30a56496e06de06b1ef750893196
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-02-08 22:14:44 +0100

    Learnt update methods

--- app/models/learnt.rb
+++ app/models/learnt.rb
@@ -3,4 +3,16 @@ class Learnt < ActiveRecord::Base
   belongs_to :word
 
   attr_accessible :value
+
+  # TODO: eliminate lost modifications
+  def right
+    update_attributes value: value + 1
+  end
+
+  def wrong
+    update_attributes value: value - 1
+  end
+
+  def didnt_know
+  end
 end
