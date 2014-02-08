ef9ef8d32ecade739c9515967b4e333713e777ae
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-02-08 13:02:51 +0100

    Words::CategoriesController - manage word categories

--- app/controllers/words/categories_controller.rb
+++ app/controllers/words/categories_controller.rb
@@ -1,6 +1,25 @@
 class Words::CategoriesController < ApplicationController
   respond_to :json
 
-  def index
+  expose(:word)
+  expose(:categories) { word.categories }
+
+  def create
+    processed_params = word_categories_params
+    word.categories = processed_params[:categories]
+    if word.save
+      render "categories/index"
+    else
+      render nothing: true, status: 409
+    end
+  end
+
+  private
+
+  def word_categories_params
+    wcp = params.permit(:word_id, categories: [])
+    wcp[:categories] ||= []
+    wcp[:categories] = wcp[:categories].map { |c| Category.find(c) }
+    wcp
   end
 end
