86484cade7f20dcd6f37e8be160dacf431b6ad8f
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-02-09 20:00:02 +0100

    UserWordSet scopes

--- app/models/user_word_set.rb
+++ app/models/user_word_set.rb
@@ -3,4 +3,13 @@ class UserWordSet < ActiveRecord::Base
   belongs_to :word_set
 
   attr_accessible :user_id, :word_set_id, :permissions
+
+  def self.select_base
+    select('user_id, word_set_id, permissions')
+  end
+
+  def self.include_user_base
+    joins(:user)
+    .select('users.first_name, users.last_name')
+  end
 end
