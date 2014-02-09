a43d89b46a251cebc15ed0f8560f0258b96e0d6b
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-02-09 19:58:53 +0100

    invitations_controller #create for creating user_word_sets from invitations

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,4 +4,15 @@ class InvitationsController < ApplicationController
   def show
   end
 
+  def create
+    word_set_ids = parse_word_set_ids(params[:word_set_ids])
+    UserWordSetsBuilder.create_by_word_set_ids(word_set_ids, invitations)
+    render "invitations/show"
+  end
+
+  private
+
+  def parse_word_set_ids word_set_ids = []
+    word_set_ids.map(&:to_i)
+  end
 end
