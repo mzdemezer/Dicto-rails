6a3fc9e9e5d75ee8efc4149a3392c3238c4d1b3b
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-02-09 22:12:18 +0100

    Add sql folder to autoload paths

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,11 @@ module DictoRails
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/app/builders #{config.root}/app/models/abstract)
+    config.autoload_paths += %W(
+      #{config.root}/app/builders
+      #{config.root}/app/models/abstract
+      #{config.root}/app/sql
+    )
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
