35fe076c54d6d62bfd39609e1438d448a5181303
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-02-09 22:12:49 +0100

    StoredProcedure abstract class

---
+++ app/sql/stored_procedure.rb
@@ -0,0 +1,49 @@
+class StoredProcedure
+  def self.drop
+    sql = drop_statement
+    execute(sql)
+  end
+
+  def self.define
+    sql = definition
+    execute(sql)
+  end
+
+  def self.call hash
+    sql = invocation(hash)
+    results = connection.execute(sql).entries
+    reconnect!
+    parse_results(results)
+  end
+
+  private
+
+  def self.drop_statement
+    throw 'implement :drop_statement method returning drop statement SQL in derived class'
+  end
+
+  def self.definition
+    throw 'implement :definition method returning definition SQL in derived class'
+  end
+
+  def self.invocation
+    throw 'implement :invocation method returning invocation SQL in derived class'
+  end
+
+  def self.parse_results results
+    results
+  end
+
+
+  def self.reconnect!
+    connection.reconnect!
+  end
+
+  def self.execute sql
+    connection.execute(sql)
+  end
+
+  def self.connection
+    ActiveRecord::Base.connection
+  end
+end
