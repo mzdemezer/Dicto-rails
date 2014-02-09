2ae295d2838e2c8b391118e0e85d7e400ec2387c
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-02-09 19:26:47 +0100

    users_controller #show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,17 @@ class UsersController < ApplicationController
   respond_to :json
 
   expose(:users)
+  expose(:user, attributes: :user_params)
 
   def index
   end
 
+  def show
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :learnt_threshold)
+  end
 end
