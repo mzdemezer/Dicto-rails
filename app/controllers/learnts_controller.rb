f67edc09e8031d9080bae4c4e5b50938477690ba
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-02-09 22:21:11 +0100

    Use Learnt::UpdateLearntProcedure

--- app/controllers/learnts_controller.rb
+++ app/controllers/learnts_controller.rb
@@ -2,22 +2,28 @@ class LearntsController < ApplicationController
   respond_to :json
 
   expose(:word_set)
-  expose(:learnt) { current_user.learnts.find_or_create_by_word_id(params[:id]) }
+  expose(:learnt) { @learnt }
 
   authorize_resource :word_set, decent_exposure: true
 
   def right
-    learnt.right
-    render "learnts/show"
+    @learnt = Learnt.right(user_id: current_user.id, word_id: params[:id])
+    render_learnt
   end
 
   def wrong
-    learnt.wrong
-    render "learnts/show"
+    @learnt = Learnt.wrong(user_id: current_user.id, word_id: params[:id])
+    render_learnt
   end
 
   def didnt_know
-    learnt.didnt_know
+    @learnt = Learnt.didnt_know(user_id: current_user.id, word_id: params[:id])
+    render_learnt
+  end
+
+  private
+
+  def render_learnt
     render "learnts/show"
   end
 end
--- app/models/learnt.rb
+++ app/models/learnt.rb
@@ -2,17 +2,26 @@ class Learnt < ActiveRecord::Base
   belongs_to :user
   belongs_to :word
 
-  attr_accessible :value
+  attr_accessible :value, :word_id, :user_id
 
-  # TODO: eliminate lost modifications
-  def right
-    update_attributes value: value + 1
+  def self.right(**attributes)
+    arguments = attributes.slice(:user_id, :word_id).merge({ dvalue: 1 })
+    update_learnt(arguments)
   end
 
-  def wrong
-    update_attributes value: value - 1
+  def self.wrong(**attributes)
+    arguments = attributes.slice(:user_id, :word_id).merge({ dvalue: -1 })
+    update_learnt(arguments)
   end
 
-  def didnt_know
+  def self.didnt_know(**attributes)
+    arguments = attributes.slice(:user_id, :word_id)
+    find_or_initialize_by_user_id_and_word_id(arguments)
+  end
+
+  private
+
+  def self.update_learnt(arguments)
+    Learnt::UpdateLearntProcedure.call(arguments)
   end
 end
