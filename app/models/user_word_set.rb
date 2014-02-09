51eb0e056aea93f1af30e38c18d585a8eb06cef9
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-02-09 19:58:33 +0100

    UserWordSet change accessible attributes

--- app/models/user_word_set.rb
+++ app/models/user_word_set.rb
@@ -2,5 +2,5 @@ class UserWordSet < ActiveRecord::Base
   belongs_to :user
   belongs_to :word_set
 
-  attr_accessible :user_id, :permissions
+  attr_accessible :user_id, :word_set_id, :permissions
 end
