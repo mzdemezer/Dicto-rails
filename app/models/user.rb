861f5d0b44cdfcc3a317b382246b8043e8879654
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-02-21 19:38:09 +0100

    Delete omniauthable for a while

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-         :trackable, :validatable,
-         :omniauthable, omniauth_providers: [ :facebook ]
+         :trackable, :validatable
 
   has_many :user_word_sets
   has_many :word_sets, through: :user_word_sets
